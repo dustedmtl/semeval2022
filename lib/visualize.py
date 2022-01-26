4eb268197df9f2dd1600cce3a6ef6fa46196fe2b
Sami Itkonen <si@iki.fi>
2022-01-26 22:38:01 +0200

    heatmap visualization code

---
+++ lib/visualize.py
@@ -0,0 +1,33 @@
+"""Data visualization."""
+
+# pylint: disable=invalid-name, line-too-long, unused-variable, unnecessary-comprehension
+
+# from typing import List, Tuple, Dict
+from typing import List, Tuple, Optional, Protocol
+import numpy as np
+import pandas as pd
+# import scipy as sp
+import seaborn as sns
+import matplotlib as plt
+from sklearn.metrics import confusion_matrix
+
+def df_heatmap(df: pd.DataFrame, gold: pd.DataFrame, col='Label') -> plt.axes:
+    """
+    Get counts and labels as a heatmap.
+    """
+    # ylabels = None
+    ylabels = df[col].unique()
+    if col != 'Label':
+        cf = confusion_matrix(df[col].map({ylabels[0]: '0', ylabels[1]: '1'}), gold['Label'])
+    else:
+        cf = confusion_matrix(df['Label'], gold['Label'])
+    # print(languages)
+    counts = [v for v in cf.flatten()]
+    pct = [v for v in cf.flatten()/np.sum(cf)]
+    labels = ["%s\n%.1f%%" % (v1, 100*v2) for v1, v2 in zip(counts, pct)]
+    labels = np.asarray(labels).reshape(2, 2)
+    if col != 'Label':
+        ax = sns.heatmap(cf, annot=labels, cmap='Blues', fmt='', yticklabels=ylabels)
+    else:
+        ax = sns.heatmap(cf, annot=labels, cmap='Blues', fmt='')
+    return ax
